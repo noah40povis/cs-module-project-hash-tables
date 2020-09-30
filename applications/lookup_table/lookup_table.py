28c399898ae0fcc6f0d6f0442b418c58dc838cb2
Noah Christian <noahchristian@Noahs-MacBook-Pro.local>
2020-09-30 15:35:38 -0400

    finished look_uptable

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,20 +1,25 @@
 # Your code here
-
+import math 
+import random
 
 def slowfun_too_slow(x, y):
-    v = math.pow(x, y)
-    v = math.factorial(v)
-    v //= (x + y)
+    v = math.pow(x, y) #x raised to the power y
+    v = math.factorial(v) #gets the factorial of 5
+    v //= (x + y) # 
     v %= 982451653
 
     return v
 
+lookup_table = {}
+
 def slowfun(x, y):
-    """
-    Rewrite slowfun_too_slow() in here so that the program produces the same
-    output, but completes quickly instead of taking ages to run.
-    """
-    # Your code here
+    #if x and y are not in the lookup table which is a cache
+    if (x, y) not in lookup_table:
+        #run slowfun_too_slow on the new x,y , but set it equal to lookup table 
+        lookup_table[(x,y)] = slowfun_too_slow(x,y)
+      #return lookup_table   
+    return lookup_table[(x,y)]
+
 
 
 
